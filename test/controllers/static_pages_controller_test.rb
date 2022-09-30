76b521d6a36069755b754e783f4115ef3b379498
hironaga.kawasaki <>
2022-09-30 09:49:05 +0900

    リファクタ

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   #必ず最初に実行する処理
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "勤怠管理システム"
   end
 
   test "should get home" do
@@ -13,23 +13,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
-
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-  end
-
-  test "should get about" do
-    get about_path
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
-  end
-
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
-
 end
